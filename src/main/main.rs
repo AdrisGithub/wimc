84df1259b09b40f91b1741fabce0430ce6338934
AdrisGithub <adrianjohn6543@gmail.com>
2023-12-25 20:20:29 +0100

    add basic structure to the main.rs

--- src/main/main.rs
+++ src/main/main.rs
@@ -1,4 +1,47 @@
-fn main() {
+use wbsl::error::WBSLError;
+use wbsl::ser_servlet::SerializeServlet;
+use wimcm::{WIMCInput, WIMCMethods, WIMCOutput};
+use wimcm::presets::{pong, respond};
+use wjp::Values;
 
+fn main() -> Result<(), WBSLError> {
+    SerializeServlet::builder()
+    .with_func(handle_requests)
+    .bind("0.0.0.0:6969")?.start();
+    Ok(())
 }
 
+fn handle_requests(input: WIMCInput) -> WIMCOutput {
+    match input.get_method() {
+        WIMCMethods::Ping => ping(),
+        WIMCMethods::Echo => echo(input),
+        WIMCMethods::Cleanup => cleanup(),
+        WIMCMethods::Store => store(input),
+        WIMCMethods::Get => get(input),
+        WIMCMethods::Query => query(input),
+    }
+}
+
+fn ping() -> WIMCOutput {
+    pong()
+}
+
+fn echo(input: WIMCInput) -> WIMCOutput {
+    respond(input.get_params().to_vec())
+}
+
+fn cleanup() -> WIMCOutput {
+    WIMCOutput::from_values(Values::Null)
+}
+
+fn store(input: WIMCInput) -> WIMCOutput {
+    todo!()
+}
+
+fn get(input: WIMCInput) -> WIMCOutput {
+    todo!()
+}
+
+fn query(input: WIMCInput) -> WIMCOutput {
+    todo!()
+}
