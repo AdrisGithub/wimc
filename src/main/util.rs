12d94c077aaf7e628049edfe7b6ce1713d881078
AdrisGithub <adrianjohn6543@gmail.com>
2023-12-30 11:32:32 +0100

    move is_due() into the util mod

--- src/main/util.rs
+++ src/main/util.rs
@@ -1,5 +1,8 @@
 use wbdl::Date;
 
-pub fn find_date(vec: &mut Vec<String>) -> Option<Date>{
+pub fn find_date(vec: &mut Vec<String>) -> Option<Date> {
     Date::try_from(vec.pop()?).ok()
+}
+pub fn is_due(date: &Date) -> bool {
+    Date::now().map(|val| &val > date).unwrap_or_default()
 }
